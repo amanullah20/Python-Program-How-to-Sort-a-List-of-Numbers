ee01c01972e26cbbcf6d413eed18db4bf4a71872
amanullah20 <67518111+amanullah20@users.noreply.github.com>
2024-10-28 16:22:27 +0600

    Update example10.py

---
+++ Data_stucture/Set/example10.py
@@ -0,0 +1,12 @@
+# Example 10 : Set length
+# The len() function is used to find the number of elements in a set .
+
+# Example set
+my_set = {1, 2, 3, 4, 5}
+
+# Getting the length of the set
+set_length = len(my_set)
+
+# Printing the length
+print('Length of set : ', set_length)
+
